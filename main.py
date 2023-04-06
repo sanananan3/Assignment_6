c3c7e1feca85a02e8c71e365d71085854ae87e3b
sanananan3 <jjy8301004@g.skku.edu>
2023-04-06 12:36:36 +0000

    trin_file_list_to_json merged

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def path_to_file_list(path: str) -> List[str]:
     return lines
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
+<<<<<<<  HEAD
     """Converts two lists of file paths into a list of json strings"""
+=======
 # Preprocess unwanted characters
 def process_file(file):
         if '\\' in file:
@@ -20,7 +22,7 @@ def process_file(file):
         if '/' or '"' in file:
             file = file.replace('/', '\\/')
             file = file.replace('"', '\\"')
-        return file
+>>>>>>>>        return file
 
     # Template for json file
     template_start = '{\"English\":\"'
